319cae1c901b3b6ed9eb89fcb0faa3130e692d74
yaonur <ya_onur@hotmail.com>
2023-11-01 14:40:38 +0300

    ss

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,38 +2,32 @@
 
 import type { Handle } from '@sveltejs/kit';
 import { init, register, locale } from 'svelte-i18n';
-import {redirect} from "@sveltejs/kit";
+import { redirect } from '@sveltejs/kit';
 
-function check_lang(lang:string|undefined){
+function check_lang(lang: string | undefined) {
 	const languages = ['en', 'tr'];
 	if (lang && !languages.includes(lang)) {
 		throw redirect(301, '/en');
 	}
-	return lang
+	return lang;
 }
 
-
 export const handle: Handle = async ({ event, resolve }) => {
-
-	let lang : string |undefined= event.params.lang
+	let lang: string | undefined = event.params.lang;
 	// console.log("<-----hooks.ts----------->")
 	// console.log("hostName:",event.url.hostname)
 	// console.log("lang:",lang)
 
-
 	register('en', () => import('$lib/i18n/lang/en.json'));
 	register('tr', () => import('$lib/i18n/lang/tr.json'));
 
-	lang=check_lang(lang)
-	
+	lang = check_lang(lang);
 
 	init({
 		fallbackLocale: 'en',
 		initialLocale: lang
 	});
-	locale.set(lang)
-
-
+	locale.set(lang);
 
 	const response = await resolve(event);
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,15 @@
 <script>
-    import NavRightContainer from '$elements/NavRightContainer.svelte';
-    // import Home from '$icon/Home.svg?component'
-    import screenSize from '$lib/stores/screenSize';
-    import {TestCompCn} from '$ui/TestCompCn';
+	import NavRightContainer from '$elements/NavRightContainer.svelte';
+	// import Home from '$icon/Home.svg?component'
+	import screenSize from '$lib/stores/screenSize';
+	import { TestCompCn } from '$ui/TestCompCn';
 
-
-    $: innerWidth = 0;
-    // $: ss=screenSize('sm')
+	$: innerWidth = 0;
+	// $: ss=screenSize('sm')
 </script>
 
-<svelte:window bind:innerWidth/>
+<svelte:window bind:innerWidth />
 
 <div class="mt-8">
-    <TestCompCn class="h-8 bg-primary" disabled="{true}"/>
+	<TestCompCn class="h-8 bg-primary" disabled={true} />
 </div>
-
-
