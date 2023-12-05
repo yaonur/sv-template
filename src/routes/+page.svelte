bce56c7a06a0dad26d06abf4d16f2cb2da969c72
yaonur <ya_onur@hotmail.com>
2023-12-05 05:08:57 +0300

    ss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-    import NavRightContainer from '$elements/NavRightContainer.svelte';
-    import Home from '$icon/Home.svg?component'
-    import screenSize from '$lib/stores/screenSize';
+	import NavRightContainer from '$elements/NavRightContainer.svelte';
+	import Home from '$icon/Home.svg?component';
+	import screenSize from '$lib/stores/screenSize';
 	import Uploader from '$lib/components/ui/Uploader/Uploader.svelte';
 
 	$: innerWidth = 0;
@@ -11,6 +11,7 @@
 <svelte:window bind:innerWidth />
 
 <div>
-    <p>Uploader</p>
-    <Uploader/>
+	<p class=" px-1 py-1">Uploader</p>
+
+	<Uploader />
 </div>
