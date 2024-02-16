4791fa093bd3d1d4d6323efec8d4a9e8886d3798
onur yanık <ya_onur@hotmail.com>
2024-02-16 22:17:59 +0300

    ss

--- src/routes/[[lang=lang]]/+page.svelte
+++ src/routes/[[lang=lang]]/+page.svelte
@@ -1,9 +1,30 @@
 <script lang="ts">
+	import { Accordion, AccordionItem } from '$ui/Accordion';
 	import { availableLanguageTags } from '$paraglide/runtime';
 	import * as m from '$paraglide/messages';
 	import { page } from '$app/stores';
 	import { route } from '$lib/i18n-routing';
+	const items = [
+		{
+			title: 'Item 0',
+			content: 'Content 0'
+		},
+		{
+			title: 'Item 1',
+			content: 'Content 1'
+		},
+		{
+			title: 'Item 2',
+			content: 'Content 2'
+		}
+	];
 </script>
-
-<p class="bg-slate-500 text-red-200">{m._test()}</p>
-<p class="text-red-200 bg-slate-500">{m._test()}</p>
+<p>Context api example with stores for comp lib </p>
+<Accordion >
+	{#each items as item, i}
+		<AccordionItem>
+			<svelte:fragment slot="title"> {item.title}</svelte:fragment>
+			<svelte:fragment slot="content"> {item.content}</svelte:fragment>
+		</AccordionItem>
+	{/each}
+</Accordion>
