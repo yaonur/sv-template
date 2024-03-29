7c286b47a281dfcd2631753945f6daf1f9271551
yaonur <ya_onur@hotmailc.om>
2024-03-30 00:42:03 +0300

    ss

--- db/supabase/functions/calculation/index.ts
+++ db/supabase/functions/calculation/index.ts
@@ -1,34 +1,84 @@
 // Follow this setup guide to integrate the Deno language server with your editor:
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.7'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.7';
 
+const url = Deno.env.get('SUPABASE_URL');
+const service_key = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY');
+const anon_key = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY');
 
-const supabase = createClient(Deno.env.get("SUPABASE_URL")!,Deno.env.get("SUPABASE_ANON_KEY")!)
+const supabase = createClient(url!, anon_key!);
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': '*',
-  }
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Headers': '*'
+};
 Deno.serve(async (req) => {
-  const {data,error} = await supabase.from("calculated").select("*").eq("id",1)
-  let resp= {}
-  if (error) {
-    console.log("Error getting name",error)
-    resp = {
-      message: `Error getting name: ${error.message}`
-    }
-  } else {
-    console.log("data:",data)
-    resp = {
-      message: data[0].calculation
-    }
-  }
-  
+	let target_user_id;
+	let calculationValue;
+	let userId = true;
+	let resp={};
+	console.log('req:', req);
+	console.log('service_key:', service_key);
 
-  return new Response(
-    JSON.stringify(resp),
-    { headers: {...corsHeaders, "Content-Type": "application/json" } },
-  )
-})
+	if (req.method === 'POST' && req.headers.get('content-type')?.includes('application/json')) {
+		try {
+			console.log('auth header:', req.headers.get('Authorization'));
+			const authHeader = req.headers.get('Authorization');
+			if (!authHeader) {
+				console.log('no auth header found');
+				return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401 });
+			}
+			const body = await req.json();
+			target_user_id = body.target_user_id;
+			calculationValue = body.calculationValue;
+			console.log('body:', body);
+			const serviceSupabase = createClient(url!, service_key!);
+      
+			if (userId) {
+				const { data: existingData, error: selectError } = await serviceSupabase
+					.from('calculated')
+					.select('*')
+					.eq('id', target_user_id)
+					.single();
+				console.log('data from select:', existingData);
+				if (selectError && selectError.details !== 'The result contains 0 rows') {
+					console.error('-------Error fetching record:', selectError);
+					return new Response(JSON.stringify({ error: selectError.message }), { status: 400 });
+				}
+				if (!existingData) {
+          console.log("no existing data inserting:")
+					const { data: insertData, error: insertError } = await serviceSupabase
+						.from('calculated') // Replace with your actual table name
+						.insert([{ id: target_user_id, value: 0 }]);
+					resp = insertError
+						? { error: insertError.message }
+						: { message: 'Record inserted', data: insertData };
+				} else {
+          console.log("updating calculated data")
+          console.log("incoming value:",calculationValue)
+          console.log("existing value:",existingData.value)
+					const newCalculatedValue = existingData.value + calculationValue;
+          console.log("calculated value:",newCalculatedValue)
+					const { data: updateData, error: updateError } = await supabase
+						.from('calculated') // Replace with your actual table name
+						.update({ value: newCalculatedValue })
+						.eq('id', target_user_id);
 
+            console.log("data after update:",updateData)
+            console.log("error on update data:",updateError)
+            resp.value = newCalculatedValue
+				}
+			}
+		} catch (error) {
+			console.error('Error parsing JSON:', error);
+			return new Response(JSON.stringify({ error: 'Bad Request' }), { status: 400 });
+		}
+	}
+
+	console.log('User ID:', userId);
+
+	return new Response(JSON.stringify({ ...resp }), {
+		headers: { ...corsHeaders, 'Content-Type': 'application/json' }
+	});
+});
--- src/routes/[[lang=lang]]/playground/+page.svelte
+++ src/routes/[[lang=lang]]/playground/+page.svelte
@@ -7,6 +7,8 @@
 	$: ({ supabase, session } = data);
 	let message = '';
 	let name = '';
+	let calculatedField=0
+	let calculationValue =1
 	let file: File | null = null; // To store the selected file
 
 	async function handleHello() {
@@ -23,14 +25,15 @@
 	}
 	async function handleCalculation() {
 		const { data, error } = await supabase.functions.invoke('calculation', {
-			body: { id: 1 }
+			body: {  calculationValue:calculationValue,target_user_id:"f5d514e2-cdae-4cfe-8669-0c7ce1a1cd9a"}
 		});
 		if (error) {
 			console.log('error:', error);
 			message = `Error ${error.message}`;
 		}
 		console.log('data:', data);
-		message = data.message;
+		message = "calculation succesfull";
+		calculatedField = data.value
 		name = data.name;
 	}
 
@@ -63,12 +66,18 @@
 	}
 </script>
 
-<div class="grid grid-cols-12 gap-4">
-	<div class="border-2 border-black p-4 col-span-3 flex flex-col gap-1">
-		<p>message: {message}</p>
-		<p>name: {name}</p>
-		<Button on:click={handleHello}>Call Hello</Button>
-		<Button on:click={handleCalculation}>Call Calculation</Button>
+<div class="grid grid-cols-12 gap-4 mx-auto">
+	<div class="border-2 w-full border-black p-4 col-span-3 flex flex-col gap-1">
+		<div class="">
+			<p>message: {message}</p>
+			<p>name: {name}</p>
+			<Button class="w-full" on:click={handleHello}>Call Hello</Button>
+		</div>
+		<div>
+			<p>Calculated Field: {calculatedField}</p>
+			<input type="text" bind:value={calculationValue}>
+			<Button on:click={handleCalculation}>Call Calculation</Button>
+		</div>
 	</div>
 	<div class="border-2 col-span-4 border-black p-5 flex flex-col gap-1">
 		<input type="file" accept="image/png" on:change={handleFileChange}>
