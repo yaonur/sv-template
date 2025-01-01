4a2d64361a442f6126169b550769d884604f034e
yaonur <ya_onur@hotmail.com>
2025-01-02 01:34:49 +0300

    ss

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,29 @@
 import type { Handle } from '@sveltejs/kit';
 import { i18n } from '$lib/i18n';
-const handleParaglide: Handle = i18n.handle();
-export const handle: Handle = handleParaglide;
+// const handleParaglide: Handle = i18n.handle();
+// export const handle: Handle = handleParaglide;
+
+export const handle: Handle = async ({ event, resolve }) => {
+	// console.log("<-----hooks.ts----------->")
+	// console.log("resolve:",resolve)
+	// console.log("event:",event)
+	// add a sample header to the response
+	const response = await i18n.handle()({ event, resolve });
+	response.headers.set('x-custom-header', 'hello world');
+	// console.log("response:",response)
+	const setCookie = response.headers.get('Set-Cookie');
+	// console.log("Set-Cookie header:", setCookie);
+	
+	if (response) {
+		return response;
+	}
+	
+	
+	
+	
+	console.log('something went wrong returning svelte response');
+	const svelteKitResponse = await resolve(event);
+	console.log("svelteKitResponse:",svelteKitResponse)
+	return svelteKitResponse;
+};
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl flex text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
